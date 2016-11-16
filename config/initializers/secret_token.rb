632b24877df6893aaa197f27acf8d6ed93853618
soplist <sopcase@yahoo.com>
2016-11-16 15:45:01 +0800

    20161116

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'fc1e46344c2f0276df94605566b23d25cfa03a793bcefb1792a3c6d88b90acdbebd35467d52fcb872836e86660c5f770dbf64c61bbad83178a6fba4593f0e1b1'
